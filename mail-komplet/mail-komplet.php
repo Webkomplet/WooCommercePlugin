ace0f115872dbaecb25df88094dd3858af58db84
brko11@gmail.com <brko11@gmail.com>
2018-01-18 15:54:33 +0100

    test, jestli je aktivni woocommerce

--- mail-komplet/mail-komplet.php
+++ mail-komplet/mail-komplet.php
@@ -31,52 +31,59 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'MAILKOMPLET_PLUGIN_NAME_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-mail-komplet-activator.php
- */
-function activate_mail_komplet() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-mail-komplet-activator.php';
-	Mail_Komplet_Activator::activate();
+ * Check if WooCommerce is active
+ **/
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    // Put your plugin code here
+    
+    /**
+     * Currently plugin version.
+     * Start at version 1.0.0 and use SemVer - https://semver.org
+     * Rename this for your plugin and update it as you release new versions.
+     */
+    define( 'MAILKOMPLET_PLUGIN_NAME_VERSION', '1.0.0' );
+    
+    /**
+     * The code that runs during plugin activation.
+     * This action is documented in includes/class-mail-komplet-activator.php
+     */
+    function activate_mail_komplet() {
+        require_once plugin_dir_path( __FILE__ ) . 'includes/class-mail-komplet-activator.php';
+        Mail_Komplet_Activator::activate();
+    }
+    
+    /**
+     * The code that runs during plugin deactivation.
+     * This action is documented in includes/class-mail-komplet-deactivator.php
+     */
+    function deactivate_mail_komplet() {
+        require_once plugin_dir_path( __FILE__ ) . 'includes/class-mail-komplet-deactivator.php';
+        Mail_Komplet_Deactivator::deactivate();
+    }
+    
+    register_activation_hook( __FILE__, 'activate_mail_komplet' );
+    register_deactivation_hook( __FILE__, 'deactivate_mail_komplet' );
+    
+    /**
+     * The core plugin class that is used to define internationalization,
+     * admin-specific hooks, and public-facing site hooks.
+     */
+    require plugin_dir_path( __FILE__ ) . 'includes/class-mail-komplet.php';
+    
+    /**
+     * Begins execution of the plugin.
+     *
+     * Since everything within the plugin is registered via hooks,
+     * then kicking off the plugin from this point in the file does
+     * not affect the page life cycle.
+     *
+     * @since    1.0.0
+     */
+    function run_mail_komplet() {
+        
+        $plugin = new Mail_Komplet();
+        $plugin->run();
+        
+    }
+    run_mail_komplet();
 }
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-mail-komplet-deactivator.php
- */
-function deactivate_mail_komplet() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-mail-komplet-deactivator.php';
-	Mail_Komplet_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_mail_komplet' );
-register_deactivation_hook( __FILE__, 'deactivate_mail_komplet' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-mail-komplet.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_mail_komplet() {
-
-	$plugin = new Mail_Komplet();
-	$plugin->run();
-
-}
-run_mail_komplet();
